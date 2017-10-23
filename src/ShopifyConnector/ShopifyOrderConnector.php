83b5a20cba0597b649a6da932e27f9cc240b74d8
dinofratelli <dinofratelli@gmail.com>
2017-10-23 12:45:07 +0200

    Shopify - Prima implementazione

---
+++ src/ShopifyConnector/ShopifyOrderConnector.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by Dino Fratelli.
+ * User: user
+ * Date: 24/02/2016
+ * Time: 15:31
+ */
+
+namespace WR\Connector\ShopifyConnector;
+
+use WR\Connector\Connector;
+use WR\Connector\ConnectorBean;
+use WR\Connector\IConnector;
+use Cake\Network\Http\Client;
+use WR\Connector\ShopifyConnection;
+use Cake\Collection\Collection;
+use Abraham\ShopifyOAuth\ShopifyOAuth;
+use PHPShopify;
+
+class ShopifyCustomerConnector extends ShopifyConnector
+{
+
+    function __construct($params)
+    {
+        parent::__construct($params);
+    }
+
+
+    /**
+     * @param null $objectId
+     * @return array
+     */
+    public function read($objectId = null)
+    {
+
+    }
+
+}
