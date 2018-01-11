7c0448e5e382fccd39d16e5aab51839e353ea7d1
Fabio Mugnano <mugnano@enterprise-consulting.it>
2018-01-11 11:42:22 +0100

    https://whiterabbit.whiterabbitsuite.com/admin/calendar_events/view/824

--- src/MagentoConnector/MagentoConnector.php
+++ src/MagentoConnector/MagentoConnector.php
@@ -13,6 +13,9 @@ use Cake\ORM\TableRegistry;
 use WR\Connector\Connector;
 use WR\Connector\IConnector;
 use App\Lib\WhiteRabbit\WRClient;
+use App\Lib\CRM\CRMManager;
+use Cake\Log\Log;
+
 
 class MagentoConnector extends Connector implements IConnector
 {
@@ -33,15 +36,21 @@ class MagentoConnector extends Connector implements IConnector
         $this->_mageuser = $params['username'];
         $this->_magepass = $params['password'];
 
+        //Log::write('debug', "magento " . serialize($params));
 
         $connectPath = $this->_mageapipath . 'connect';
+
 
         $response = $this->_http->post($connectPath, [
             'username' => $this->_mageuser,
             'password' => $this->_magepass
         ]);
 
+
         $this->_magetoken = json_decode($response->body)->token;
+
+
+        //$this->_magetoken="ciao";
 
     }
 
@@ -79,18 +88,18 @@ class MagentoConnector extends Connector implements IConnector
     /**
      * @return array
      */
-    public function write($content)
-    {
-        $publishPath = $this->_mageapipath . '/publish';
-        $response = $this->_http->post($publishPath, [
-            'type' => 'newsletter',
-            'content' => '',
-            'content_id' => ''
-        ]);
+    /*   public function write($content)
+       {
+           $publishPath = $this->_mageapipath . '/publish';
+           $response = $this->_http->post($publishPath, [
+               'type' => 'newsletter',
+               'content' => '',
+               'content_id' => ''
+           ]);
 
-        return false;
+           return false;
 
-    }
+       }*/
 
     public function update($content, $objectId)
     {
@@ -98,7 +107,7 @@ class MagentoConnector extends Connector implements IConnector
 
     public function delete($objectId = null)
     {
-        if($this->_magetoken != null) {
+        if ($this->_magetoken != null) {
             $publishPath = $this->_mageapipath . 'delete';
             $response = $this->_http->post($publishPath, [
                 'type' => 'post',
@@ -112,7 +121,8 @@ class MagentoConnector extends Connector implements IConnector
         }
     }
 
-    public function mapFormData($data) {
+    public function mapFormData($data)
+    {
         return $data;
     }
 
@@ -131,9 +141,61 @@ class MagentoConnector extends Connector implements IConnector
 
     }
 
+    /**
+     * @param $content
+     * @return bool|mixed
+     */
     public function add_user($content)
     {
+        //It's not correct to implement this here. Trying to find a different solutions
+//        $nlRecipientLists = TableRegistry::get('MarketingTools.MtNewsletterRecipientLists');
+//        $listId = null;
+//        if (isset($content['list_name'])) {
+//            $listId = $nlRecipientLists->saveFromConnector(
+//                $content['list_name'], $content['customer_id']);
+//        }
+//
+//        $nlRecipients = TableRegistry::get('MarketingTools.MtNewsletterRecipients');
+//        $nlRecipient = $nlRecipients->newEntity();
 
+        $data = [];
+        $data['externalid'] = $this->notSetToEmptyString($content['customer_id']);
+        $data['companyname'] = $this->notSetToEmptyString($content['customer_id']);
+        $data['firstname'] = $this->notSetToEmptyString($content['name']);
+        $data['lastname'] = $this->notSetToEmptyString($content['surname']);
+        $data['email1'] = $this->notSetToEmptyString($content['email']);
+        $data['mobilephone1'] = $this->notSetToEmptyString($content['mobilephone1']);
+        $data['site_name'] = $this->notSetToEmptyString($content['site_name']);
+
+        //extra
+
+
+        $data['address'] = $this->notSetToEmptyString($content['address']);
+        $data['city'] = $this->notSetToEmptyString($content['city']);
+        $data['postalcode'] = $this->notSetToEmptyString($content['postalcode']);
+        $data['province'] = $this->notSetToEmptyString($content['province']);
+        $data['birthdaydate'] = $this->notSetToEmptyString($content['birthdaydate']);
+        $data['telephone1'] = $this->notSetToEmptyString($content['telephone1']);
+        $data['taxcode'] = $this->notSetToEmptyString($content['taxcode']);
+        $data['nation'] = $this->notSetToEmptyString($content['nation']);
+        //$data['operation'] = $this->notSetToEmptyString($content['operation']);
+        $data['newsletter_subscription_date'] = $this->notSetToEmptyString($content['newsletter_subscription_date']);
+        $data['newsletter_subscription_ip'] = $this->notSetToEmptyString($content['newsletter_subscription_ip']);
+        $data['typeid'] = $this->notSetToEmptyString($content['typeid']);
+        $data['contact_typeid'] = $this->notSetToEmptyString($content['contact_typeid']);
+
+        try {
+            //actionid =  $data['typeid']  . $data['operation']
+            $crmManager = new CRMManager();
+            $data['typeid'] = $crmManager::$ecommerceTypeId;
+            $data['operation'] = $crmManager::$ecommerceActionAddUserId;
+
+            $cmrRes = $crmManager->pushClientToCrm($content['customer_id'], $data);
+
+            return $cmrRes;
+        } catch (\PDOException $e) {
+            return false;
+        }
     }
 
     public function captureFan($objectId = null)
@@ -150,7 +212,7 @@ class MagentoConnector extends Connector implements IConnector
             ->first();
 
         if ($ciChannel && isset($content['categories_tree'])) {
-            $ciChannel->categories_tree = json_encode ($content['categories_tree']);
+            $ciChannel->categories_tree = json_encode($content['categories_tree']);
             try {
                 return $ciChannels->save($ciChannel);
             } catch (\PDOException $e) {
@@ -161,16 +223,40 @@ class MagentoConnector extends Connector implements IConnector
         return false;
     }
 
-    /**
-     * @return bool
-     */
-    public function isLogged()
-    {
 
+    private function notSetToEmptyString(&$myString)
+    {
+        return (!isset($myString)) ? '' : $myString;
     }
 
-    public function callback($params)
+
+    public function write($content)
     {
-//
+        /*$data = array(
+            'orderIdExt' => '100',
+            'sourceId' => 'magento',
+            'orderNum' => '100',
+            'orderDate' => '2016-11-30',
+            'orderTotal' => '100.10'
+        );*/
+        $data = [];
+        $data['orderIdExt'] = $this->notSetToEmptyString($content['orderIdExt']);
+        $data['sourceId'] = $this->notSetToEmptyString($content['sourceId']);
+        $data['orderNum'] = $this->notSetToEmptyString($content['orderNum']);
+        $data['orderDate'] = $this->notSetToEmptyString($content['orderDate']);
+        $data['orderTotal'] = $this->notSetToEmptyString($content['orderTotal']);
+        $data['email'] = $this->notSetToEmptyString($content['email']);
+        $data['orderState'] = $this->notSetToEmptyString($content['orderState']);
+        $data['orderNote'] = $this->notSetToEmptyString($content['orderNote']);
+        $data['site_name'] = $this->notSetToEmptyString($content['site_name']);
+        $data['productActivity'] = $this->notSetToEmptyString(unserialize($content['productActivity']));
+        try {
+            $crmManager = new CRMManager();
+            $cmrRes = $crmManager->pushOrderToCrm($content['customer_id'], $data);
+
+            return $cmrRes;
+        } catch (\PDOException $e) {
+            return false;
+        }
     }
 }
---
+++ src/MagentoConnector/MagentoEcommerceConnector.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Created by Dino Fratelli / Mugnano Fabio.
+ * User: user
+ * Date: 24/02/2016
+ * Time: 15:31
+ */
+
+namespace WR\Connector\MagentoConnector;
+
+
+use WR\Connector\Connector;
+use WR\Connector\IConnector;
+use Cake\ORM\TableRegistry;
+use App\Lib\CRM\CRMManager;
+
+class MagentoEcommerceConnector extends MagentoConnector
+{
+
+    public function __construct($params) {
+        parent::__construct($params);
+    }
+
+    /**
+     * @param $content
+     * @return bool
+     */
+    public function write($content)
+    {
+        /*$data = array(
+            'orderIdExt' => '100',
+            'sourceId' => 'magento',
+            'orderNum' => '100',
+            'orderDate' => '2016-11-30',
+            'orderTotal' => '100.10'
+        );*/
+        $data = [];
+        $data['orderIdExt'] = $this->notSetToEmptyString($content['orderIdExt']);
+        $data['sourceId'] = $this->notSetToEmptyString($content['sourceId']);
+        $data['orderNum'] = $this->notSetToEmptyString($content['orderNum']);
+        $data['orderDate'] = $this->notSetToEmptyString($content['orderDate']);
+        $data['orderTotal'] =  $this->notSetToEmptyString($content['orderTotal']);
+        $data['email'] =  $this->notSetToEmptyString($content['email']);
+        $data['orderState'] =  $this->notSetToEmptyString($content['orderState']);
+        $data['orderNote'] =  $this->notSetToEmptyString($content['orderNote']);
+        $data['site_name'] = $this->notSetToEmptyString($content['site_name']);
+        $data['productActivity'] =  (unserialize($content['productActivity']));
+        try {
+            $crmManager = new CRMManager();
+            $cmrRes = $crmManager->pushOrderToCrm($content['customer_id'], $data);
+
+            return $cmrRes;
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param null $objectId
+     * @return array|null
+     */
+    public function read($objectId = null)
+    {
+        if ($objectId == null) {
+            return [];
+        }
+
+        return $objectId;
+    }
+
+
+    /**
+     * @param $content
+     * @param $objectId
+     * @return mixed
+     */
+    public function update($content, $objectId)
+    {
+        return $content;
+    }
+
+
+    /**
+     * @param $content
+     * @return bool|\Cake\Datasource\EntityInterface|mixed
+     */
+    public function add_user($content)
+    {
+        //It's not correct to implement this here. Trying to find a different solutions
+//        $nlRecipientLists = TableRegistry::get('MarketingTools.MtNewsletterRecipientLists');
+//        $listId = null;
+//        if (isset($content['list_name'])) {
+//            $listId = $nlRecipientLists->saveFromConnector(
+//                $content['list_name'], $content['customer_id']);
+//        }
+//
+//        $nlRecipients = TableRegistry::get('MarketingTools.MtNewsletterRecipients');
+//        $nlRecipient = $nlRecipients->newEntity();
+
+        $data = [];
+        $data['externalid'] = $this->notSetToEmptyString($content['customer_id']);
+        $data['companyname'] = $this->notSetToEmptyString($content['customer_id']);
+        $data['firstname'] = $this->notSetToEmptyString($content['name']);
+        $data['lastname'] = $this->notSetToEmptyString($content['surname']);
+        $data['email1'] =  $this->notSetToEmptyString($content['email']);
+        $data['mobilephone1'] = $this->notSetToEmptyString($content['mobilephone1']);
+        $data['site_name'] = $this->notSetToEmptyString($content['site_name']);
+
+        //extra
+
+
+        $data['address'] = $this->notSetToEmptyString($content['address']);
+        $data['city'] = $this->notSetToEmptyString($content['city']);
+        $data['postalcode'] = $this->notSetToEmptyString($content['postalcode']);
+        $data['province'] = $this->notSetToEmptyString($content['province']);
+        $data['birthdaydate'] = $this->notSetToEmptyString($content['birthdaydate']);
+        $data['telephone1'] = $this->notSetToEmptyString($content['telephone1']);
+        $data['taxcode'] = $this->notSetToEmptyString($content['taxcode']);
+        $data['nation'] = $this->notSetToEmptyString($content['nation']);
+        //$data['operation'] = $this->notSetToEmptyString($content['operation']);
+        $data['newsletter_subscription_date'] = $this->notSetToEmptyString($content['newsletter_subscription_date']);
+        $data['newsletter_subscription_ip'] = $this->notSetToEmptyString($content['newsletter_subscription_ip']);
+        $data['typeid'] = $this->notSetToEmptyString($content['typeid']);
+        $data['contact_typeid'] = $this->notSetToEmptyString($content['contact_typeid']);
+
+        try {
+            //actionid =  $data['typeid']  . $data['operation']
+            $crmManager = new CRMManager();
+            $data['typeid'] = $crmManager::$ecommerceTypeId;
+            $data['operation'] = $crmManager::$ecommerceActionAddUserId;
+            $cmrRes = $crmManager->pushClientToCrm($content['customer_id'], $data);
+
+            return $cmrRes;
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
+
+    private function notSetToEmptyString (&$myString) {
+        return (!isset($myString)) ? '' : $myString;
+    }
+
+}
