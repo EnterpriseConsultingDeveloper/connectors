1a4bef34127f9c522c1adba084f1d34f25be7c49
Fabio Mugnano <mugnano@enterprise-consulting.it>
2018-12-11 14:48:09 +0100

    Ps Contact Connector

--- src/PrestashopConnector/PrestashopContactConnector.php
+++ src/PrestashopConnector/PrestashopContactConnector.php
@@ -17,7 +17,8 @@ use App\Lib\CRM\CRMManager;
 class PrestashopContactConnector extends PrestashopConnector
 {
 
-    public function __construct($params) {
+    public function __construct($params)
+    {
         parent::__construct($params);
     }
 
@@ -26,7 +27,7 @@ class PrestashopContactConnector extends PrestashopConnector
      */
     public function write($content)
     {
-        if($this->_wptoken != null) {
+        if ($this->_wptoken != null) {
             $publishPath = $this->_wpapipath . 'publish';
             $response = $this->_http->post($publishPath, [
                 'type' => 'newsletter',
@@ -85,43 +86,73 @@ class PrestashopContactConnector extends PrestashopConnector
 //        $nlRecipients = TableRegistry::get('MarketingTools.MtNewsletterRecipients');
 //        $nlRecipient = $nlRecipients->newEntity();
 
+
         $data = [];
         $data['externalid'] = $this->notSetToEmptyString($content['customer_id']);
         //$data['companyname'] = $this->notSetToEmptyString($content['customer_id']);
         $data['firstname'] = $this->notSetToEmptyString($content['name']);
         $data['lastname'] = $this->notSetToEmptyString($content['surname']);
-        $data['email1'] =  $this->notSetToEmptyString($content['email']);
-        $data['mobilephone1'] = $this->notSetToEmptyString($content['mobile']);
-        $data['telephone1'] = $this->notSetToEmptyString($content['telephone1']);
-        $data['operation'] = $this->notSetToEmptyString($content['operation']);
-        $data['newsletter_subscription_date'] = $this->notSetToEmptyString($content['newsletter_subscription_date']);
-        $data['newsletter_subscription_ip'] = $this->notSetToEmptyString($content['newsletter_subscription_ip']);
-        $data['typeid'] = $this->notSetToEmptyString($content['typeid']);
-        $data['contact_typeid'] = $this->notSetToEmptyString($content['contact_typeid']);
+        $data['email1'] = $this->notSetToEmptyString($content['email']);
+        //  $data['mobilephone1'] = $this->notSetToEmptyString($content['mobile']);
+        //  $data['telephone1'] = $this->notSetToEmptyString($content['telephone1']);
+        //  $data['operation'] = $this->notSetToEmptyString($content['operation']);
+        //  $data['newsletter_subscription_date'] = $this->notSetToEmptyString($content['newsletter_subscription_date']);
+        //  $data['newsletter_subscription_ip'] = $this->notSetToEmptyString($content['newsletter_subscription_ip']);
+        //  $data['typeid'] = $this->notSetToEmptyString($content['typeid']);
+        //  $data['contact_typeid'] = $this->notSetToEmptyString($content['contact_typeid']);
         $data['site_name'] = $this->notSetToEmptyString($content['site_name']);
+        $data['id_order_str'] = $this->notSetToEmptyString($content['id_order_str']);
         $data['description'] = $this->notSetToEmptyString($content['message']);
         $data['title'] = $this->notSetToEmptyString($content['title']);
 
         try {
-            //nlRecipients->saveFromConnector($nlRecipient);
-
-            //if($res) {
-            //$cmrRes = $this->pushToCrm($content['customer_id'], $res);
             $crmManager = new CRMManager();
             $crmManager->setCustomer($content['customer_id']);
-           // $cmrRes = $crmManager->pushClientToCrm($content['customer_id'], $data);
-            $cmrRes = $crmManager->pushSalesTicketToCrm($content['customer_id'], $data);
 
-            //debug($cmrRes); die;
-            //}
-            return $cmrRes;
+            if (!$crmManager->retrieveCustomerClient($content['customer_id'], $content['email'])) {
+                $data['typeid'] = $crmManager::$ecommerceTypeId;
+                $data['operation'] = $crmManager::$ecommerceActionAddUserId;
+                $cmrRes = $crmManager->pushClientToCrm($content['customer_id'], $data);
+            }
+
+
+            $dataAct = array(
+                'typeid' => $crmManager::$newslettertLandingPagid,
+                'source' => $crmManager::$newslettertLandingPageSource,
+                'sourceid' => $content['email'],
+                'actionid' => $crmManager::$newslettertLandingPageSubmit,
+                //'title' => $crmManager::$ecommerceTypeId . $crmManager::$crmSeparator . $crmManager::$ecommerceActionContactId,
+                'note' => $crmManager::$ecommerceTypeId . $crmManager::$crmSeparator . $crmManager::$ecommerceActionContactId . " on " . $data['site_name']  .   $data['id_order_str'] . " : " .    $data['description'] ,
+                'properties' => serialize($data)
+            );
+
+            $crmManager->pushActivityoCrm($content['customer_id'], $dataAct);
+
+            if ($content['ticket'] == 1) {
+                $cmrRes = $crmManager->pushSalesTicketToCrm($content['customer_id'], $data, null, false);
+            }
+
+
+            return !empty($cmrRes) ? $cmrRes : true;
         } catch (\PDOException $e) {
             return false;
         }
     }
 
-    private function notSetToEmptyString (&$myString) {
+    private function notSetToEmptyString(&$myString)
+    {
         return (!isset($myString)) ? '' : $myString;
+    }
+
+
+    /**
+     * @param $content
+     * @param $objectId
+     * @return mixed
+     */
+    public function write_cart($content)
+    {
+        return $content;
     }
 
 }
