7e93e570188b78aa7b2c3807453b663558b73e56
Leandro <leandro.cesinaro@gmail.com>
2020-04-23 12:55:59 +0200

    Form Custom Mapping & Optimizing captureFan + Pagination

--- src/FacebookConnector/FacebookAdsConnector.php
+++ src/FacebookConnector/FacebookAdsConnector.php
@@ -57,40 +57,41 @@ class FacebookAdsConnector extends FacebookConnector
             $adsArray['account_name'] = $accountName;
             $adsArray['account_status'] = $accountStatus;
 
-            //TODO: if isset campaigns
-            if ($this->fb->next($graphEdge)) {
-                $campaignsArray = $graphEdge->asArray();
-                $result += $campaignsArray;
-
-                while ($graphEdge = $this->fb->next($graphEdge)) {
+            if (isset($graphEdge)) {
+                if ($this->fb->next($graphEdge)) {
                     $campaignsArray = $graphEdge->asArray();
-                    $result = array_merge($result, $campaignsArray);
+                    $result += $campaignsArray;
+
+                    while ($graphEdge = $this->fb->next($graphEdge)) {
+                        $campaignsArray = $graphEdge->asArray();
+                        $result = array_merge($result, $campaignsArray);
+                    }
+
+                } else {
+                    $campaignsArray = $graphEdge->asArray();
+                    $result += $campaignsArray;
                 }
 
-            } else {
-                $campaignsArray = $graphEdge->asArray();
-                $result += $campaignsArray;
-            }
 
+                foreach ($result as $key => $elem) {
+                    $val = Hash::extract($result, $key . '.status');
+                    $result = Hash::remove($result, $key . '.status');
+                    $result = Hash::insert($result, $key . '.campaign_status', $val[0]);
 
-            foreach($result as $key => $elem) {
-                $val = Hash::extract($result, $key. '.status');
-                $result = Hash::remove($result, $key.'.status');
-                $result = Hash::insert($result, $key.'.campaign_status', $val[0]);
+                    $val = Hash::extract($result, $key . '.id');
+                    $result = Hash::remove($result, $key . '.id');
+                    $result = Hash::insert($result, $key . '.campaign_id', $val[0]);
 
-                $val = Hash::extract($result, $key.'.id');
-                $result = Hash::remove($result, $key.'.id');
-                $result = Hash::insert($result, $key.'.campaign_id', $val[0]);
+                    $val = Hash::extract($result, $key . '.name');
+                    $result = Hash::remove($result, $key . '.name');
+                    $result = Hash::insert($result, $key . '.campaign_name', $val[0]);
 
-                $val = Hash::extract($result, $key.'.name');
-                $result = Hash::remove($result, $key.'.name');
-                $result = Hash::insert($result, $key.'.campaign_name', $val[0]);
+                    $val = Hash::extract($result, $key . '.created_time');
+                    $result = Hash::remove($result, $key . '.created_time');
+                    $result = Hash::insert($result, $key . '.campaign_date', $val[0]);
 
-                $val = Hash::extract($result, $key.'.created_time');
-                $result = Hash::remove($result, $key.'.created_time');
-                $result = Hash::insert($result, $key.'.campaign_date', $val[0]);
-
-                $result[$key] += $adsArray;
+                    $result[$key] += $adsArray;
+                }
             }
 
         } catch (\Facebook\Exceptions\FacebookResponseException $e) {
@@ -107,9 +108,6 @@ class FacebookAdsConnector extends FacebookConnector
      */
     public function captureFan($objectId = null) {
         // Read complete lead gen
-        if ($objectId == null)
-            $objectId = $this->objectAdsId;
-
         if ($objectId == null) {
             return [];
         }
@@ -118,62 +116,100 @@ class FacebookAdsConnector extends FacebookConnector
         $forms_lang = Config::load( __DIR__ . '/lang', new Json)->all();
         $iso_lang = array_keys($forms_lang);
 
+        //TODO: time range between last cron executed
+        if (!empty($this->since) && !empty($this->until)) {
+            /*da finire*/
+            //$limitString = '.since(' . $this->since . ').until(' . $this->until.")";
+        }
+
         try {
 
-            $streamToRead = '/' . $objectId . '?fields=campaigns{adsets{ads{status,leads{field_data,form_id,platform,ad_id,ad_name,adset_id,adset_name,campaign_id,campaign_name,created_time}},status}},account_status';
-            $response = $this->fb->sendRequest('GET', $streamToRead);
-
-            $data = $response->getDecodedBody()['campaigns'];
+            $streamToRead = '/' . $objectId . '?fields=adsets{ads{leads{field_data,form_id,platform,ad_id,ad_name,adset_id,adset_name,campaign_id,campaign_name,created_time}}}';
+            $request = $this->fb->request('GET', $streamToRead);
+            $graphNodes = $this->fb->getClient()->sendRequest($request);
+            $graphEdgeAdSet = $graphNodes->getGraphNode()->getField('adsets');
 
             // Append users that have taken lead gen
             $social_users = array();
             $dataCrm = array();
             $form_ids = array();
 
-            foreach ($data['data'] as $d) {
-                if (isset($d['adsets'])) {
-                    foreach ($d['adsets']['data'] as $ad_set) {
-                        if (isset($ad_set['ads'])) {
-                            foreach ($ad_set['ads']['data'] as $ads) {
-                                if (isset($ads['leads'])) {
-                                    foreach ($ads['leads']['data'] as $key => $leads) {
-                                        if (!in_array($leads['form_id'],array_keys($form_ids))) {
-                                            //GET locale of Forms
-                                            $streamToRead = '/' . $leads['form_id'] . '?fields=locale';
-                                            $response = $this->fb->sendRequest('GET', $streamToRead);
-                                            //check if exist lang on folder
-                                            if(!isset($forms_lang) || !in_array($response->getDecodedBody()['locale'],$iso_lang)){
-                                                continue;
+            if (isset($graphEdgeAdSet)) {
+                do {
+                    foreach ($graphEdgeAdSet as $adSet) {
+                        $graphEdgeAd = $adSet->getField('ads');
+                        if (isset($graphEdgeAd)) {
+                            do {
+                                foreach ($graphEdgeAd as $ads) {
+                                    $graphEdgeLead = $ads->getField('leads');
+                                    if (isset($graphEdgeLead)) {
+                                        do {
+                                            foreach ($graphEdgeLead as $key => $leads) {
+                                                $form_id = $leads->getField('form_id');
+                                                $lead_id = $leads->getField('id');
+                                                if (!in_array($form_id, array_keys($form_ids))) {
+                                                    //GET locale of Forms
+                                                    $streamToRead = '/' . $form_id . '?fields=locale';
+                                                    $response = $this->fb->sendRequest('GET', $streamToRead);
+                                                    //check if exist lang on folder
+                                                    if (!isset($forms_lang) || !in_array($response->getDecodedBody()['locale'],
+                                                            $iso_lang)) {
+                                                        continue;
+                                                    }
+                                                    $fields_lang = $forms_lang[$response->getDecodedBody()['locale']];
+
+                                                    $form_ids[$form_id]['locale'] = $response->getDecodedBody()['locale'];
+                                                    $form_ids[$form_id]['field_maps'] = $fields_lang;
+                                                    $form_ids[$form_id]['error'] = false;
+                                                }
+
+                                                //form already mapped wrong
+                                                if ($form_ids[$form_id]['error']) {
+                                                    continue;
+                                                }
+
+                                                foreach ($leads->getField('field_data')->asArray() as $form_field) {
+                                                    $social_users[$form_field['name']] = $form_field['values'][0];
+                                                }
+
+                                                $mapData = UtilitiesComponent::remap($social_users,
+                                                    $form_ids[$form_id]['field_maps'], false);
+
+                                                //Custom Mapping Form from FB
+                                                if (empty($mapData)) {
+                                                    $fields_lang = $forms_lang['custom'];
+                                                    $form_ids[$form_id]['field_maps'] = $fields_lang;
+
+                                                    $mapData = UtilitiesComponent::remap($social_users,
+                                                        $form_ids[$form_id]['field_maps'], false);
+                                                    if (empty($mapData)) {
+                                                        $form_ids[$form_id]['error'] = true;
+                                                        continue;
+                                                    }
+                                                }
+
+                                                //Mapping Field Form and CRM for all Lang
+                                                $dataCrm[$lead_id] = $mapData;
+                                                $dataCrm[$lead_id] += [
+                                                    "date_add" => $leads->getField('created_time'),
+                                                    "action" => "lead",
+                                                    "contentId" => $leads->getField('ad_id'),
+                                                    "platform" => $leads->getField('platform'),
+                                                    "ad_name" => $leads->getField('ad_name'),
+                                                    "id" => $lead_id,
+                                                    "adset_name" => $leads->getField('adset_name'),
+                                                    "campaign_name" => $leads->getField('campaign_name'),
+                                                ];
                                             }
-                                            $fields_lang = $forms_lang[$response->getDecodedBody()['locale']];
-
-                                            $form_ids[$leads['form_id']]['locale'] = $response->getDecodedBody()['locale'];
-                                            $form_ids[$leads['form_id']]['field_maps'] = $fields_lang;
-                                        }
-
-                                        foreach ($leads['field_data'] as $form_field){
-                                            $social_users[$form_field['name']] = $form_field['values'][0];
-                                        }
-
-                                        //Mapping Field Form and CRM for all Lang
-                                        $dataCrm[$key] = UtilitiesComponent::remap($social_users, $form_ids[$leads['form_id']]['field_maps'], false);
-                                        $dataCrm[$key] += [
-                                            "date_add" => $leads['created_time'],
-                                            "action" => "lead",
-                                            "contentId" => $leads['ad_id'],
-                                            "platform" => $leads['platform'],
-                                            "ad_name" => $leads['ad_name'],
-                                            "id" => $leads['id'],
-                                            "adset_name" => $leads['adset_name'],
-                                            "campaign_name" => $leads['campaign_name'],
-                                        ];
+                                        } while ($graphEdgeLead = $this->fb->next($graphEdgeLead));
                                     }
                                 }
-                            }
+                            } while ($graphEdgeAd = $this->fb->next($graphEdgeAd));
                         }
                     }
-                }
+                } while ($graphEdgeAdSet = $this->fb->next($graphEdgeAdSet));
             }
+
             $data['social_users'] = $dataCrm;
         } catch (\Facebook\Exceptions\FacebookResponseException $e) {
             $data = [];
