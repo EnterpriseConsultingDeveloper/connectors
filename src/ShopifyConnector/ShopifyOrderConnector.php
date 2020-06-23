670e7444b9ea03647334028860cb437449cdcee5
Fabio Mugnano <mugnano@enterprise-consulting.it>
2020-06-23 17:17:53 +0200

    new shopify api https://kanboard.whiterabbitsuite.eu/project/3/task/5092

--- src/ShopifyConnector/ShopifyOrderConnector.php
+++ src/ShopifyConnector/ShopifyOrderConnector.php
@@ -37,83 +37,83 @@ class ShopifyOrderConnector extends ShopifyConnector
     {
         $params_call = array();
         $params_call['status'] = "any";
+        $params_call['limit'] = $this->limitCall;
         if (!empty($params['date'])) {
             $params_call['created_at_min'] = $params['date'];
         }
         \Cake\Log\Log::debug('Shopify ShopifyOrderConnector call read on ' . $params['shop_url'] . ' params ' . print_r(json_encode($params_call), true));
-        try {
-            $count_order_db = $this->shopify->Order->count($params_call);
-        } catch (Exception $e) {
-            $count_order_db = 0;
-        }
+        $count_order_db = $this->shopify->Order->count($params_call);
         \Cake\Log\Log::debug('Shopify ShopifyOrderConnector call read on ' . $params['shop_url'] . ' count_order_db ' . $count_order_db);
-        $exit = 0;
-        $count_order_crm = 0;
-        $page = 0;
-        while ($exit == 0) {
-            $page++;
-            $params_call['limit'] = $this->limitCall;
-            $params_call['page'] = $page;
-            $orders = $this->shopify->Order->get($params_call);
-            foreach ($orders as $order) {
-                \Cake\Log\Log::debug('Shopify ShopifyOrderConnector import order_number ' . $order['order_number']);
-                $data = [];
-                $data['source'] = $this->shopUrl;
-                $data['email'] = $this->notSetToEmptyString($order['email']);
-                $data['number'] = $this->notSetToEmptyString($order['order_number']);
-                $data['orderdate'] = $order['created_at'];
-                $data['order_status'] = $this->notSetToEmptyString($order['financial_status']);
-                $data['total'] = $this->notSetToEmptyString($order['total_price']);
-                $data['currency'] = $this->notSetToEmptyString($order['currency']);
-                $data['tax_total'] = $this->notSetToEmptyString($order['total_tax']);
-                $data['subtotal'] = $this->notSetToEmptyString($order['subtotal_price']);
-                $data['cart_discount'] = $this->notSetToEmptyString($order['total_discounts']);
+        $orderResource = $this->shopify->Order();
+        $orders = $orderResource->get($params_call);
+        $count_order_crm = count($orders);
+        \Cake\Log\Log::debug('Shopify ShopifyOrderConnector call orders count ' . $count_order_crm . ' on ' . $params['shop_url']);
+
+        $nextPageOrders = $orderResource->getNextPageParams();
+        $nextPageOrdersArray = [];
+        while ($nextPageOrders) {
+            $nextPageOrdersArray = $orderResource->get($orderResource->getNextPageParams());
+            $count_order_crm += count($nextPageOrdersArray);
+            \Cake\Log\Log::debug('Shopify ShopifyOrderConnector call count_order_crm count ' . $count_order_crm . ' on ' . $params['shop_url']);
+            $orders = array_merge($orders, $nextPageOrdersArray);
+            $nextPageOrders = $orderResource->getNextPageParams();
+        }
+
+        foreach ($orders as $order) {
+            \Cake\Log\Log::debug('Shopify ShopifyOrderConnector import order_number ' . $order['order_number']);
+            $data = [];
+            $data['source'] = $this->shopUrl;
+            $data['email'] = $this->notSetToEmptyString($order['email']);
+            $data['number'] = $this->notSetToEmptyString($order['order_number']);
+            $data['orderdate'] = $order['created_at'];
+            $data['order_status'] = $this->notSetToEmptyString($order['financial_status']);
+            $data['total'] = $this->notSetToEmptyString($order['total_price']);
+            $data['currency'] = $this->notSetToEmptyString($order['currency']);
+            $data['tax_total'] = $this->notSetToEmptyString($order['total_tax']);
+            $data['subtotal'] = $this->notSetToEmptyString($order['subtotal_price']);
+            $data['cart_discount'] = $this->notSetToEmptyString($order['total_discounts']);
 
 //          $data['shipping_total'] = $this->notSetToEmptyString($content['shipping_total']);
-                $data['shipping_firstname'] = $this->notSetToEmptyString($order['shipping_address']['shipping_firstname']);
-                $data['shipping_lastname'] = $this->notSetToEmptyString($order['shipping_address']['last_name']);
-                $data['shipping_address'] = $this->notSetToEmptyString($order['shipping_address']['address1']);
-                $data['shipping_postalcode'] = $this->notSetToEmptyString($order['shipping_address']['zip']);
-                $data['shipping_city'] = $this->notSetToEmptyString($order['shipping_address']['city']);
-                $data['shipping_country'] = $this->notSetToEmptyString($order['shipping_address']['country']);
-                $data['shipping_phone'] = $this->notSetToEmptyString($order['shipping_address']['phone']);
+            $data['shipping_firstname'] = $this->notSetToEmptyString($order['shipping_address']['shipping_firstname']);
+            $data['shipping_lastname'] = $this->notSetToEmptyString($order['shipping_address']['last_name']);
+            $data['shipping_address'] = $this->notSetToEmptyString($order['shipping_address']['address1']);
+            $data['shipping_postalcode'] = $this->notSetToEmptyString($order['shipping_address']['zip']);
+            $data['shipping_city'] = $this->notSetToEmptyString($order['shipping_address']['city']);
+            $data['shipping_country'] = $this->notSetToEmptyString($order['shipping_address']['country']);
+            $data['shipping_phone'] = $this->notSetToEmptyString($order['shipping_address']['phone']);
 //          $data['shipping_tax'] = $this->notSetToEmptyString($order['shipping_address']['address1']);
 
-                $data['payment_method'] = $this->notSetToEmptyString($order['gateway']);
+            $data['payment_method'] = $this->notSetToEmptyString($order['gateway']);
 //          $data['shipping_method'] = $this->notSetToEmptyString($content['shipping_method']);
-                /*new*/
-                $data['description'] = $this->notSetToEmptyString($order['note']);
-                $data['products'] = array();
-                $data['tags'] = array();
+            /*new*/
+            $data['description'] = $this->notSetToEmptyString($order['note']);
+            $data['products'] = array();
+            $data['tags'] = array();
 
-                foreach ($order['line_items'] as $id => $product) {
+            foreach ($order['line_items'] as $id => $product) {
 //            debug($product);
-                    $data['products'][$id]['product_id'] = $product['id'];
-                    $data['products'][$id]['name'] = $product['title'];
-                    $data['products'][$id]['qty'] = $product['quantity'];
-                    $data['products'][$id]['price'] = $product['price'];
-                    $data['products'][$id]['discount'] = $product['total_discount'];
-                    /*new*/
-                    $data['products'][$id]['sku'] = $this->notSetToEmptyString($product['sku']);
-                    $data['products'][$id]['description'] = $this->notSetToEmptyString($product['name']);
-                    $data['products'][$id]['tax'] = $this->notSetToEmptyString($product['tax_lines'][0]['price']);
-                }
-                try {
-                    $this->createCrmConnection($customerId);
-                    $changeStatusBean = new ActivityEcommerceChangeStatusBean();
-                    $changeStatusBean->setCustomer($customerId)
-                        ->setSource($this->shopUrl)
-                        ->setToken($this->shopUrl)
-                        ->setDataRaw($data);
-                    ActionsManager::pushOrder($changeStatusBean);
-                } catch (\Exception $e) {
-                    // Log error
-                }
-
+                $data['products'][$id]['product_id'] = $product['id'];
+                $data['products'][$id]['name'] = $product['title'];
+                $data['products'][$id]['qty'] = $product['quantity'];
+                $data['products'][$id]['price'] = $product['price'];
+                $data['products'][$id]['discount'] = $product['total_discount'];
+                /*new*/
+                $data['products'][$id]['sku'] = $this->notSetToEmptyString($product['sku']);
+                $data['products'][$id]['description'] = $this->notSetToEmptyString($product['name']);
+                $data['products'][$id]['tax'] = $this->notSetToEmptyString($product['tax_lines'][0]['price']);
             }
-            $count_order_crm += $this->limitCall;
-            if ($count_order_crm >= $count_order_db) {
-                $exit = 1;
+            try {
+                \Cake\Log\Log::debug('Shopify ShopifyOrderConnector call ActivityEcommerceAddUserBean by ' . $data['email'] . ' on ' . $params['shop_url']);
+
+                $this->createCrmConnection($customerId);
+                $changeStatusBean = new ActivityEcommerceChangeStatusBean();
+                $changeStatusBean->setCustomer($customerId)
+                    ->setSource($this->shopUrl)
+                    ->setToken($this->shopUrl)
+                    ->setDataRaw($data);
+                ActionsManager::pushOrder($changeStatusBean);
+            } catch (\Exception $e) {
+                // Log error
             }
         }
         return true;
